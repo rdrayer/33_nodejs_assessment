7eb05c5fd3044b368d4aa4d84d7b7ca163dfb164
Roberta Drayer <robertadrayer@gmail.com>
2023-11-22 15:04:11 -0800

    fixed broken app

--- broken-app/app.js
+++ broken-app/app.js
@@ -1,18 +1,26 @@
 const express = require('express');
-let axios = require('axios');
-var app = express();
+const axios = require('axios');
 
-app.post('/', function(req, res, next) {
+const app = express();
+app.use(express.json());
+
+app.post('/', async function(req, res, next) {
   try {
-    let results = req.body.developers.map(async d => {
-      return await axios.get(`https://api.github.com/users/${d}`);
-    });
+    // promise.all to wait for all requests to complete
+    let results = await Promise.all(
+      req.body.developers.map(async d => {
+        let response = await axios.get(`https://api.github.com/users/${d}`);
+        return response.data;
+      })
+    );
     let out = results.map(r => ({ name: r.data.name, bio: r.data.bio }));
 
-    return res.send(JSON.stringify(out));
-  } catch {
+    return res.json(out); //use res.json to send json response
+  } catch (err){
     next(err);
   }
 });
 
-app.listen(3000);
+app.listen(3000, function() {
+  console.log('app listening on port 3000');
+});
--- broken-app/issues.md
+++ broken-app/issues.md
@@ -1,1 +1,6 @@
 # Broken App Issues
+-missing callback in app.listen
+-not parsing incoming JSON requests. needed bc we're
+using req.body.developers
+-not using async for promises
+-missing error handling in catch block
